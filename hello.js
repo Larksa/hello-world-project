0b449eac68f69b96185e84f07adb284f4075047e
Larkey <43057189+Larksa@users.noreply.github.com>
2025-03-26 20:14:28 +1100

    Add JavaScript Hello World implementation

---
+++ hello.js
@@ -0,0 +1,11 @@
+/**
+ * A simple Hello World program in JavaScript
+ */
+
+// Function to print Hello World
+function helloWorld() {
+    console.log("Hello, World!");
+}
+
+// Call the function
+helloWorld();
