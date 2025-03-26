9e355b2862d181bf09f2dc87afd52de2d8e49bd6
Larkey <43057189+Larksa@users.noreply.github.com>
2025-03-26 20:15:02 +1100

    Add C++ Hello World implementation

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+/**
+ * A simple Hello World program in C++
+ */
+#include <iostream>
+
+// Main function that prints Hello World
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
