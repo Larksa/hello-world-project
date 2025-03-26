22e56057a08996d95e03912d3af2f586f4b34ef7
Larkey <43057189+Larksa@users.noreply.github.com>
2025-03-26 20:14:38 +1100

    Add Java Hello World implementation

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+ * A simple Hello World program in Java
+ */
+public class Hello {
+    /**
+     * Main method that prints Hello World
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
